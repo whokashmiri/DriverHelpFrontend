73ec35a7dea97f6f68e3659fb32a9beef73d0926
Aaqib Bashir Mir <aaqibmir.ab@gmail.com>
2026-09-17 12:53:01 +0300

    app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+import appJson from "./app.json";
+
+export default {
+  expo: {
+    ...appJson.expo,
+
+    plugins: [
+      ...(appJson.expo.plugins ?? []),
+
+      "@maplibre/maplibre-react-native",
+    ],
+  },
+};
